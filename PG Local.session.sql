55576a48ac30bc0edfb16fa6c9230aac28718cbb
chandana <chandanasapumal21@gmail.com>
2025-01-15 15:17:25 +0530

    Greet Response

---
+++ PG Local.session.sql
@@ -0,0 +1,17 @@
+SELECT * FROM student;
+
+
+INSERT INTO student (
+    first_name,
+    last_name,
+    email,
+    age,
+    enrollment_date
+  )
+VALUES (
+    'chandana',
+    'sapumal',
+    'chandansapumal21@gmiail.com',
+    25,
+    '2000-01-03'
+  );
