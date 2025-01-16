86de6831d7565522a1ab3f617aeb71076ac6c241
chandana <chandanasapumal21@gmail.com>
2025-01-17 02:00:39 +0530

    exception hanle for notify the user about the correct error in API calls

--- demo/src/main/java/com/chandana/Main.java
+++ demo/src/main/java/com/chandana/Main.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication(scanBasePackages = "com.chandana.customer")
-public class Main { 
+public class Main {  
    
     public static void main(String[] args){
        // CustomerService customerService = new CustomerService(new CustomerDataAccessService());
